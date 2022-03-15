657021836085b201ab519a19339bec592bdc3f3f
fathur rizky <61815491+rzkyfathur@users.noreply.github.com>
2022-03-15 21:39:30 +0700

    membuat contoh1 controller

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Fathur Rizky
+			Saya tingga di daerah Parung Bogor
+			olah raga yang saya sukai adalah
+			Bulutangkis";
+	}
+}
