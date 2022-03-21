31aa88b5543ab939ccca23f11749c9f45ccd5914
fathur rizky <61815491+rzkyfathur@users.noreply.github.com>
2022-03-21 13:33:45 +0700

    latihan pert 2

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $route['default_controller'] = 'welcome';
 $route['contoh1'] = 'contoh1';
+// $route['view-latihan'] = 'view-latihan';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
